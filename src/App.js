4331a943bdb1d6176ee6411829a93a5f1d3ff3a9
root <lemenct@gmail.com>
2019-02-10 17:26:42 +0100

    Add Grommet component library

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet } from 'grommet'
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '16px',
+      height: '20px',
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={theme}>
+        App
+      </Grommet>
     );
   }
 }
